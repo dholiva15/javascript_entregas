b65a4be1125d636785e42504e3fb149cbd643e90
Diego Oliva <dieg81@gmail.com>
2022-04-26 11:58:21 -0300

    primera entrega

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+ alert ("bienvenidos al bar clandestino");
+ alert ("Clave: tenista frances");
+ let clave= prompt("ingrese la clave");
+
+ if((clave === "René Lacoste")|| (clave==="rené lacoste"))
+ {alert ("ingresaste una clave valida");
+ alert ("su reserva es aceptada");
+ let nombre= prompt("ingrese su nombre y apellido")
+ alert ("lo esperam@s a partir de las 21 hs")
+} 
+  else {alert("ingresaste una clave incorrecto. Hasta la proxima semana");}
+
