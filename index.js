275cf43403f37ddb8fff10c940614b9446b2c501
Diego Oliva <dieg81@gmail.com>
2022-05-12 18:48:31 -0300

    Tercera entrega

--- index.js
+++ index.js
@@ -13,10 +13,11 @@
 } 
   else {alert("ingresaste una clave incorrecto. Hasta la proxima semana");}*/
 
+  
   // Primera entrega del desafio
 
 
-
+/*
 
 const destino_nacional = [
 
@@ -59,5 +60,117 @@ else if(opcionesViajes==="Internacional"){
 for (const viaje of destino_internacional){
   console.log(viaje.destino)
 }
+
+
+*/
+
+
+// Nos preparamos para la entrega del Desafio Incorporar Eventos
+
+
+
+
+
+
+const contenedor = document.getElementById("viajes");
+const tablaCarrito = document.getElementById("tablaCarrito");
+
+
+
+const carrito = [];
+
+
+const PRODUCTOS = [
+    {
+        id: 1,
+        nombre: "Menoza",
+        precio: 30000,
+        
+        imagen: "https://i0.wp.com/www.disfrutarosario.com/wp-content/uploads/2020/12/Mendoza.jpg?w=700&ssl=1"
+    },
+    {
+        id: 2,
+        nombre: "Ushuaia",
+        precio: 50000,
+        
+        imagen: "https://lavidadeviaje.com/wp-content/uploads/2019/10/lavidadeviaje-ushuaia-tierra-del-fuego-portada-3870.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Salta",
+        precio: 25000,
+        
+        imagen: "https://media.staticontent.com/media/pictures/85fc1916-3476-47c3-95b8-5172feb08ab4/1120x532?op=TRUNCATE&enlarge=false&gravity=sm&quality=80&dpr=2"
+    }
+];
+
+
+
+const getCard = (item) => {
+  return (`
+  <div class="card" style="width: 18rem;">
+  <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+  <div class="card-body">
+    <h5 class="card-title">${item.nombre}</h5>
+    <p class="card-text">$${item.precio}</p>
+    <a href="#" onclick=agregarAlCarrito(${item.id}) class="btn btn-primary">agregar</a>
+  </div>
+</div>
+  `);
+}
+
+const getRow = (item) =>{
+  return(
+    `
+    <tr>
+      <th scope="row">${item.id}</th>
+      <td>${item.nombre}</td>
+      <td>${item.cantidad}</td>
+      <td>${item.precio}</td>
+      <td><img style="width:20px" src="${item.imagen}" alt="imagen"</td>
+
+    </tr>
+    
+    `
+
+  )
+}
+
+const cargarProductos= (datos, nodo, esTabla) =>{
+  let acumulador = "";
+  datos.forEach((el)=>{
+    acumulador += esTabla ? getRow(el) : getCard(el);
+  })
+  nodo.innerHTML = acumulador;
+  
+
+}
+
+
+const agregarAlCarrito = (id) =>{
+  const seleccion = PRODUCTOS.find(item => item.id===id);
+  const busqueda = carrito.findIndex(el => el.id === id);
+  console.log(busqueda)
+  if(busqueda === -1) {
+    alert("agregaste un viaje a:  " + seleccion.nombre)
+    carrito.push({ 
+      id: seleccion.id,
+      nombre: seleccion.nombre,
+      precio: seleccion.precio,
+      cantidad:1,
+      imagen: seleccion.imagen,
+    })
+
+
+  } else{
+    carrito[busqueda].cantidad = carrito[busqueda].cantidad+ 1
+  }
+ 
+  cargarProductos(carrito, tablaCarrito, true);
+}
+
+cargarProductos(PRODUCTOS, contenedor, false)
+
+
 
 
